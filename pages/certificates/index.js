512f5fe1a2acb665f6ec9b1dcb1e12cf0e015145
Reyhanizadeh <-->
2023-05-18 03:14:10 +0330

    certificate size and profile has been edited (c.p)

--- pages/certificates/index.js
+++ pages/certificates/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Footer from "@/components/Footer/Footer";
 import Meta from "@/components/Meta";
 import backGround from "../../styles/images/background.svg";
+import styles from "../../styles/Certificate.module.css";
 import avatar from "../../styles/images/avatar.png";
 import certificate1 from "../../styles/images/certificate1.png";
 import certificate2 from "../../styles/images/certificate2.png";
@@ -29,9 +30,11 @@ export default function Certificates() {
             <p className="textGray pt-0 mt-1">since 2016</p>
           </div>
         </div>
-        <h5 className={`title1 text-center mt-5 pt-5`}>Certificates</h5>
-        <h1 className={`title2 text-center mt-3`}>My Certificates</h1>
-        <div className="d-flex flex-column flex-md-row flex-wrap mx-5 mt-3 justify-content-around">
+        <h5 className={`title1 text-center mt-2 pt-2`}>Certificates</h5>
+        <h1 className={`title2 text-center mt-1`}>My Certificates</h1>
+        <div
+          className={`d-flex flex-column flex-md-row flex-wrap mx-auto mt-1 justify-content-around ${styles.certificateContainer}`}
+        >
           <Certificate
             src={certificate1}
             alt="certificate Blender"
